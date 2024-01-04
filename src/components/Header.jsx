831b7bf176f09e20f92edbfdd346668a02cd41e7
Gabriel Alves <gabriel52.alves@gmail.com>
2024-01-04 10:12:56 -0300

    style: Refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from "react";
 import './Header.css'
 import '../index'
+import calc from '../assets/images/calc.svg'
 
 export default class Header extends Component {
     render() {
         return (
             <div>
                 <div className="header" >
-                    <p data-aos="zoom-in-up" data-duration="2500">Calculadora em React</p>
+                    <img className="calc-logo" src={calc} alt="calc" /> 
+                    <p data-aos="zoom-in-up" data-duration="2500">Calculadora - React JS</p>
                 </div>
             </div>
         )
